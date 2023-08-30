4ccfaa8ff384f6571d448c6692a6da085a0c2240
vyadir <yadir.sve@gmail.com>
2023-08-29 23:43:05 -0600

    Reto 3 machote

---
+++ Reto 3.py
@@ -0,0 +1,18 @@
+"""
+    Creado por Yadir Vega Espinoza 
+    Curso: Programación lineal
+    Objetivo: Repaso básico de python
+
+    Reto 1:
+            Construya una función llamada EcuacionCuadratica(a,b,c) con a diferente de cero y retorne el código LaTeX del 
+            procedimiento que resuelve la ecuación ax^2+bx+c=0, en IR, debe incluir las explicaciones y las habladas, sean 
+            lo más didácticos que les sea posible sin exagerar. Conciso pero entendible y bien explicado.
+
+    Fecha: 2023-08-29
+"""
+
+
+import sympy as sp
+
+def EcuacionCuadratica(a, b, c):
+    pass
